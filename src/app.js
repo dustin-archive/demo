1c8e0420bc252a12b50faf83d715f14fd092359a
Dustin Dowell <dustindowell22@gmail.com>
2017-11-03 16:04:22 -0500

    add some navigation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,44 @@
 'use strict'
 
-import { app } from 'hyperapp'
+import { h, app } from 'hyperapp'
 import { Router } from './Router'
 import { RouterView } from './RouterView'
 
-app({
+const Nav = (state, actions) =>
+  h('ul', null, [
+    h('li', null, [
+      h('a', { href: '#/about' }, 'about')
+    ]),
+    h('li', null, [
+      h('a', { href: '#/contact' }, 'contact')
+    ]),
+    h('li', null, [
+      h('a', { href: '#/dashboard' }, 'dashboard'),
+      h('ul', null, [
+        h('li', null, [
+          h('a', { href: '#/dashboard' }, 'dashboard profile')
+        ]),
+        h('li', null, [
+          h('a', { href: '#/dashboard' }, 'dashboard listings')
+        ]),
+        h('li', null, [
+          h('a', { href: '#/dashboard' }, 'dashboard settings')
+        ])
+      ])
+    ]),
+    h('li', null, [
+      h('a', { href: '#/404' }, 'not found')
+    ])
+  ])
+
+const hyperapp = app({
   modules: { Router },
-  view: (state, actions) => RouterView(state, actions)
+  view: (state, actions) =>
+    h('div', null, [
+      Nav(state, actions),
+      h('br'),
+      RouterView(state, actions)
+    ])
 })
 
 // Subscriptions
