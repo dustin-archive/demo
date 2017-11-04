908c478c00817927820a8bb0bc4e69a88fdb825d
Dustin Dowell <dustindowell22@gmail.com>
2017-11-03 19:07:53 -0500

    add more routes

--- src/RouterView.js
+++ src/RouterView.js
@@ -1,11 +1,15 @@
 import { h } from 'hyperapp'
 
 const Home = h('div', null, [
-  h('div', null, 'home is where i live')
+  h('div', null, 'home')
 ])
 
 const About = h('div', null, [
   h('div', null, 'about')
+])
+
+const Catalog = h('div', null, [
+  h('div', null, 'catalog')
 ])
 
 const Contact = h('div', null, [
@@ -16,6 +20,26 @@ const Dashboard = h('div', null, [
   h('div', null, 'dashboard')
 ])
 
+const Inventory = h('div', null, [
+  h('div', null, 'inventory')
+])
+
+const Post = h('div', null, [
+  h('div', null, 'post')
+])
+
+const Profile = h('div', null, [
+  h('div', null, 'profile')
+])
+
+const Settings = h('div', null, [
+  h('div', null, 'settings')
+])
+
+const Update = h('div', null, [
+  h('div', null, 'update')
+])
+
 const NotFound = h('div', null, [
   h('div', null, '404')
 ])
@@ -23,8 +47,14 @@ const NotFound = h('div', null, [
 const RouterView = (state, actions) => ({
   '': Home,
   '/about': About,
+  '/catalog': Catalog,
   '/contact': Contact,
-  '/dashboard': Dashboard
+  '/dashboard': Dashboard,
+  '/inventory': Inventory,
+  '/post': Post,
+  '/profile': Profile,
+  '/settings': Settings,
+  '/update': Update
 })[state.Router.path] || NotFound
 
 export { RouterView }
