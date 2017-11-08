2b2f38e8fd9cb1234a801bc9865768df889c77e8
Dustin Dowell <dustindowell22@gmail.com>
2017-11-08 16:52:43 -0600

    remove old code

--- src/views/App.js
+++ src/views/App.js
@@ -1,27 +1,10 @@
 import { h } from 'picodom'
 
 const App = (state, actions, views, data) => {
-  console.log(state, actions, views, data)
   return h('div', null, [
     h('div', null, 'howdy'),
     h('br'),
     views.Code(state)
-    // views.Search(),
-    // views.Nav(),
-    // views.Router(),
-    // h('br', null, null),
-    // views.Drop('foo')(
-    //   h('div', null, 'foo')
-    // ),
-    // views.Drop('bar')(
-    //   h('div', null, 'bar')
-    // ),
-    // views.Drop('baz')(
-    //   h('div', null, 'baz')
-    // ),
-    // views.Drop('qux')(
-    //   h('div', null, 'qux')
-    // ),
   ])
 }
 
