5e7f24b11b7fbfa1f02ec2ce39d779d78fe3c71a
Dustin Dowell <dustindowell22@gmail.com>
2017-11-08 15:50:39 -0600

    import things properly

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 'use strict'
 
 import app from './paperapp'
-import stores from './stores'
-import views from './views'
+import * as stores from './stores'
+import * as views from './views'
 
-app({ stores, views })
+app({
+  stores,
+  views,
+  init (_, actions) {
+    actions.Overlay.init()
+    actions.Router.init()
+  }
+})
