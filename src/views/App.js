9f1d640c1f680d122a83268e07f3a51da2748460
Dustin Dowell <dustindowell22@gmail.com>
2017-11-08 21:38:25 -0600

    add buttons to change route

--- src/views/App.js
+++ src/views/App.js
@@ -3,7 +3,20 @@ import { h } from 'picodom'
 const App = (state, actions, views, data) => {
   return h('div', null, [
     h('div', null, 'howdy'),
-    h('br'),
+    h('button', {
+      onclick () {
+        actions.Router.route({
+          path: '/foo'
+        })
+      }
+    }, 'foo'),
+    h('button', {
+      onclick () {
+        actions.Router.route({
+          path: '/bar'
+        })
+      }
+    }, 'bar'),
     views.Code(state)
   ])
 }
