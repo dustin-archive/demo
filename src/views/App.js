cca95c80145ba63409edab6b9aca01b5bf7f0f49
Dustin Dowell <dustindowell22@gmail.com>
2017-11-08 22:30:50 -0600

    better app demo

--- src/views/App.js
+++ src/views/App.js
@@ -2,21 +2,31 @@ import { h } from 'picodom'
 
 const App = (state, actions, views, data) => {
   return h('div', null, [
-    h('div', null, 'howdy'),
+    h('h1', null, 'Router:'),
     h('button', {
       onclick () {
-        actions.Router.route({
-          path: '/foo'
-        })
+        actions.Router.route({ path: '/foo', query: { foo: 'bar' } })
       }
     }, 'foo'),
+    h('br'),
     h('button', {
       onclick () {
-        actions.Router.route({
-          path: '/bar'
-        })
+        actions.Router.route({ path: '/baz', query: { qux: 'corge' } })
       }
-    }, 'bar'),
+    }, 'baz'),
+    h('br'),
+    h('h1', null, 'Drop:'),
+    views.Drop('one')(
+      h('div', null, 'one')
+    ),
+    views.Drop('two')(
+      h('div', null, 'two')
+    ),
+    views.Drop('three')(
+      h('div', null, 'three')
+    ),
+    h('br'),
+    h('h1', null, 'State:'),
     views.Code(state)
   ])
 }
